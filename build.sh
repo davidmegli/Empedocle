4e70fe3fa6c26aa90f8a8a33a7bca9853fbf1892
Sara Bernini <sara.bernini.21@gmail.com>
2025-05-21 12:44:32 +0200

    update

---
+++ build.sh
@@ -0,0 +1,8 @@
+cd stlab-modules
+mvn clean install -DskipTests
+cd ../empedocle/empedocle-viewers
+mvn clean install -DskipTests
+cd ../../empedocle/stlab-woodelements
+mvn clean install -DskipTests
+cd ../../empedocle/empedocle
+mvn clean install -DskipTests
