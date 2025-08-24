71c2c35877bd3516a50dbd9b3e02f7432fe657ef
David Megli <78218248+davidmegli@users.noreply.github.com>
2025-08-24 18:38:02 +0200

    Added Secured imports

--- empedocle/stlab-woodelements/src/main/java/it/unifi/ing/stlab/woodelements/api/WoodElementResource.java
+++ empedocle/stlab-woodelements/src/main/java/it/unifi/ing/stlab/woodelements/api/WoodElementResource.java
@@ -8,6 +8,7 @@ import it.unifi.ing.stlab.woodelements.factory.WoodElementFactory;
 import it.unifi.ing.stlab.woodelements.manager.WoodElementManager;
 import it.unifi.ing.stlab.woodelements.model.WoodElement;
 import it.unifi.ing.stlab.users.model.User;
+import it.unifi.ing.stlab.empedocle.security.Secured;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
